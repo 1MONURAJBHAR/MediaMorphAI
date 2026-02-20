937420d1bd509ca552cd9bacd5b8fc9ff1978353
Monu Rajbhar <rajbharmonu436@gmail.com>
2026-02-20 21:16:51 +0530

    Add public routes and create basic page components for home, social share, video upload, sign-in, and sign-up

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,13 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const isPublicRoute = createRouteMatcher(["/signin", "/signup", "/", "/home"]);
+const isPublicRoute = createRouteMatcher([
+    "/sign-in",
+    "/sign-up",
+    "/",
+    "/home"
+]);
+
 const isPublicApiRoute = createRouteMatcher(["/api/videos"]);
 
 export default clerkMiddleware(async (auth, req) => {
@@ -19,12 +25,12 @@ export default clerkMiddleware(async (auth, req) => {
   if (!userId) {
     //if user is not logged in and trying to access a proctected route then redirect them to signin page
     if (!isPublicRoute(req) && !isPublicApiRoute(req)) {
-      return NextResponse.redirect(new URL("/signin", req.url));
+      return NextResponse.redirect(new URL("/sign-in", req.url));
     }
 
     //if the request is for a proctected API and the user is not logged in then redirect them to signin page
     if (isApiRequest && !isPublicApiRoute(req)) {
-      return NextResponse.redirect(new URL("/signin", req.url));
+      return NextResponse.redirect(new URL("/sign-in", req.url));
     }
   }
 

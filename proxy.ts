3fe7d9c3a512049a555b7e408331368c8b70cfa7
Monu Rajbhar <rajbharmonu436@gmail.com>
2026-02-20 18:37:23 +0530

    Initialize project and add Clerk authentication middleware and integrate DaisyUI themes

---
+++ proxy.ts
@@ -0,0 +1,48 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+const isPublicRoute = createRouteMatcher(["/signin", "/signup", "/", "/home"]);
+const isPublicApiRoute = createRouteMatcher(["/api/videos"]);
+
+export default clerkMiddleware(async (auth, req) => {
+  const { userId } = await auth();
+  const currentUrl = new URL(req.url);
+  const isHomePage = currentUrl.pathname === "/home";
+  const isApiRequest = currentUrl.pathname.startsWith("/api");
+
+  //user logged in and accessing public routes like signin, signup and is not on home page then redirect him to homepage
+  if (userId && isPublicRoute(req) && !isHomePage) {
+    return NextResponse.redirect(new URL("/home", req.url));
+  }
+
+  //user not logged in
+  if (!userId) {
+    //if user is not logged in and trying to access a proctected route then redirect them to signin page
+    if (!isPublicRoute(req) && !isPublicApiRoute(req)) {
+      return NextResponse.redirect(new URL("/signin", req.url));
+    }
+
+    //if the request is for a proctected API and the user is not logged in then redirect them to signin page
+    if (isApiRequest && !isPublicApiRoute(req)) {
+      return NextResponse.redirect(new URL("/signin", req.url));
+    }
+  }
+
+  return NextResponse.next();
+});
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
+
+
+/*
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
+};*/
+

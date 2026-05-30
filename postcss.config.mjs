ef861995cd6675e6b16d479e5c8dbaa53b8139a8
Monu Rajbhar <rajbharmonu436@gmail.com>
2026-05-31 00:13:30 +0530

    update postcss file

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,16 @@
-const config = {
+/*const config = {
   plugins: {
     "@tailwindcss/postcss": {},
   },
 };
 
 export default config;
+*/
+
+const postcssConfig = {
+  plugins: {
+    "@tailwindcss/postcss": {},
+  },
+};
+
+export default postcssConfig;
